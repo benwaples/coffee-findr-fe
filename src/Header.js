a4fe2747619c1c33547a71be553b03cef89e5301
bwapes <benwaples@gmail.com>
2020-08-27 08:37:59 -0700

    edit files for rerouting

--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,9 @@ export default class Header extends Component {
   render() {
     return (
       <header>
-        <div className="links"><a href="./">Login</a><a href="./coffeeList">Search</a></div>
+        <div className="links"><a href="./">Login</a><a href="../CoffeeList/CoffeeList.js">Search</a></div>
         <h1 className="appName">No Stars, No Bucks</h1>
-        <div className="links"><a href="./favorites">Favorites</a><a href="google.com">Coffee 101</a></div>
+        <div className="links"><a href="./Favorites">Favorites</a><a href="google.com">Coffee 101</a></div>
       </header>
     )
   }
