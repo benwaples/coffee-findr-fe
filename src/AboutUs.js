15202b80e98852c561439c770312445a41668c81
bwapes <benwaples@gmail.com>
2020-08-27 15:03:27 -0700

    style and edit about us

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -9,18 +9,18 @@ export default class AboutUs extends Component {
           <div className="left">
           <section>
             <h3>Brain</h3>
-            <img src="/coffeeCups.png" className="brain cup" alt="Brian"/>
-            <p>Least Favorite Coffee Drink: Machiatto</p>
-            <p>Favorite Coffee Shop: Gas Station</p>
+            <img src="/avataaars(1).png" className="avatar" alt="Brian"/>
+            <p><strong>Least Favorite Coffee Drink:</strong> Americano</p>
+            <p><strong><strong>Favorite Coffee Shop:</strong></strong> Gas Station</p>
             <a href="https://www.linkedin.com/in/brian-mcphillips-75a00639/" rel="noopener noreferrer" target="_blank">
               <img src="/linkedin-icon.png" alt="linkedin"/>
             </a>
           </section>
           <section>
             <h3>Nate</h3>
-            <img src="/coffeeCups.png" className="nate cup" alt="Nate"/>
-            <p>Least Favorite Coffee Drink: Anything from Starbucks</p>
-            <p>Favorite Coffee Shop: not Starbucks</p>
+            <img src="/avataaars(2).png" className="avatar" alt="Nate"/>
+            <p><strong>Least Favorite Coffee Drink:</strong> Anything from Starbucks</p>
+            <p><strong>Favorite Coffee Shop:</strong> not Starbucks</p>
             <a href="https://www.linkedin.com/in/nathan-kruckenberg-974620193/" rel="noopener noreferrer" target="_blank">
               <img src="/linkedin-icon.png"alt="linkedin"/>
             </a>
@@ -29,9 +29,9 @@ export default class AboutUs extends Component {
           <div className="middle">
           <section>
             <h3>Joel</h3>
-            <img src="/coffeeCups.png" className="joel cup" alt="Joel"/>
-            <p>Least Favorite Coffee Drink: Kurig Coffee</p>
-            <p>Favorite Coffee Shop: Nossa Familia</p>
+            <img src="/joel_avataaars.png" className="avatar" alt="Joel"/>
+            <p><strong>Least Favorite Coffee Drink:</strong> Kurig Coffee</p>
+            <p><strong>Favorite Coffee Shop:</strong> Nossa Familia</p>
             <a href="https://www.linkedin.com/in/joelrperez/" rel="noopener noreferrer" target="_blank">
               <img src="/linkedin-icon.png"alt="linkedin"/>
             </a>
@@ -40,18 +40,18 @@ export default class AboutUs extends Component {
           <div className="right">
           <section>
             <h3>Adrian</h3>
-            <img src="/coffeeCups.png" className="Adrian cup" alt="Adrian"/>
-            <p>Least Favorite Coffee Drink: Whats coffee?</p>
-            <p>Favorite Coffee Shop: The ones in Amsterdam </p>
+            <img src="/adrian.png" className="avatar" alt="Adrian"/>
+            <p><strong>Least Favorite Coffee Drink:</strong> Whats coffee?</p>
+            <p><strong>Favorite Coffee Shop:</strong> The ones in Amsterdam </p>
             <a href="https:/www.linkedin.com/in/adrian-simental/" rel="noopener noreferrer" target="_blank">
               <img src="/linkedin-icon.png" alt="linkedin" />
             </a>
           </section>
           <section>
             <h3>Ben</h3>
-            <img src="/coffeeCups.png" className="Ben cup" alt="Ben"/>
+            <img src="/ben.png" className="avatar" alt="Ben"/>
             <p>Least Favorite Coffee Shop Drink: Red Bull Italian Soda</p>
-            <p>Favorite Coffee Shop: Camber</p>
+            <p><strong>Favorite Coffee Shop:</strong> Camber</p>
             <a href="https://www.linkedin.com/in/ben-waples-b04796161/" rel="noopener noreferrer" target="_blank">
               <img src="/linkedin-icon.png" alt="linkedin"/>
             </a>
